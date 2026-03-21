62c723722382b71cb1824341dafbefa71fdd2eca
cheikhg577-eng <cheikhg577@gmail.com>
2026-03-21 23:46:04 +0000

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const axios = require("axios");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  if (req.method === "OPTIONS") return res.status(200).send();
+  next();
+});
 
 const PAYDUNYA_MASTER_KEY  = "zSI0Bxkh-IJEk-d0Ik-ERUf-fJYa52ZoIelK";
 const PAYDUNYA_PRIVATE_KEY = "test_private_qpNlrED1SSnIKNVcUYtKylt9V62";
