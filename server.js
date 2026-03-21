4090a4b434faea968389dca8d5ed008b003b3108
cheikhg577-eng <cheikhg577@gmail.com>
2026-03-21 22:38:42 +0000

    Update server.js

--- server.js
+++ server.js
@@ -127,5 +127,4 @@ app.post("/paydunya-webhook", async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log("Serveur démarré sur le port 3000"));
-}
+app.listen(process.env.PORT || 3000, () => console.log("Serveur démarré"));
